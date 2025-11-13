5758c335449ec43dbbc124c59d9dfefb2a4799db
Brandner <brandner@akaryon.com>
2025-11-13 20:07:28 +0100

    DesignHTML

---
+++ src/Controller/DesignController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DesignController extends AbstractController
+{
+    #[Route('/design', name: 'app_design')]
+    public function index(): Response
+    {
+        return $this->render('design/index.html.twig', [
+            'controller_name' => 'DesignController',
+        ]);
+    }
+}
